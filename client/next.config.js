6b5740d95c990efef1e004932ca1badfcd29277b
beomJoon <104752645+joon-github@users.noreply.github.com>
2022-10-26 19:36:45 +0900

    수정

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
-// import withImages from "next-images";
-// modules.exports = withImages({
-//   webpack(config, options) {
-//     return config;
-//   },
-// });
-// export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// };
+import withImages from "next-images";
+modules.exports = withImages({
+  webpack(config, options) {
+    return config;
+  },
+});
+export default nextConfig;
