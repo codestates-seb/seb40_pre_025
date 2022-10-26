7bbf353e87feef6dfd0e066203dc435eacb3ced1
beomJoon <104752645+joon-github@users.noreply.github.com>
2022-10-26 17:33:38 +0900

    MyPage 추가 작성

---
+++ client/pages/users/[id]/saves/qustions.js
@@ -0,0 +1,35 @@
+import CoreInfo from "../coreInfo";
+import Saves_qustions from "../../../../components/userPageComponents/saves_qustions";
+import { useRouter } from "next/router";
+export default function saves_qustions() {
+  const router = useRouter();
+  return (
+    <div className="container">
+      <div className="content">
+        <CoreInfo></CoreInfo>
+        <Saves_qustions></Saves_qustions>
+        <style jsx>{`
+          .container {
+            height: 100vh;
+            width: 100vh;
+            display: flex;
+            justify-content: space-between;
+            flex: 1 0 auto;
+            position: relative;
+            margin: 0;
+            text-align: left;
+          }
+          .content {
+            width: 50%;
+            min-width: 515px;
+            padding: 24px;
+            margin: 0;
+            border-radius: 0;
+            border-top-width: 0;
+            border-bottom-width: 0;
+          }
+        `}</style>
+      </div>
+    </div>
+  );
+}
