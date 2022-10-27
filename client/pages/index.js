ced09cd8709abf75f1fef506286cbd4a5a92bb4b
anjisu <guajs0818@gmail.com>
2022-10-27 18:53:35 +0900

    컴포넌트

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,24 +1,9 @@
-import Header from "../components/Header";
-import Header2 from "../components/Header2";
-import Github from "../components/Github";
-import Github2 from "../components/Github2";
-import Google from "../components/Google";
-import Google2 from "../components/Google2";
-import SideBar from "../components/SideBar";
-import Footer from "../components/Footer";
-
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <Header2 />
-      <Google />
-      <Google2 />
-      <Github />
-      <Github2 />
-      <SideBar />
-      <Footer />
     </div>
   );
 }
