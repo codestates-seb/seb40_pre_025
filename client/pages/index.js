a8bf2d3b373b02a11a11b9ace36282e78391ae80
anjisu <guajs0818@gmail.com>
2022-10-27 19:07:30 +0900

    컴포넌트

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,12 @@
-import Header from "./components/Header";
+import Header from "../components/Header";
+import Header2 from "../components/Header2";
+import Sidebar from "../components/SideBar"
 
 export default function Home() {
   return (
     <div>
-      <Header />
+      
+      <Sidebar />
     </div>
   );
 }
