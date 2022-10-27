2b5a0f231087f1fe918cdb35068a56fad8a77a7a
anjisu <guajs0818@gmail.com>
2022-10-27 09:53:50 +0900

    후터 수정

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -13,6 +13,7 @@ const Footer = () => {
                           <path d="m21.5 0-2.7 2 9.9 13.3 2.7-2L21.5 0ZM26 18.4 13.3 7.8l2.1-2.5 12.7 10.6-2.1 2.5ZM9.1 15.2l15 7 1.4-3-15-7-1.4 3Zm14 10.79.68-2.95-16.1-3.35L7 23l16.1 2.99ZM23 30H7v-3h16v3Z" fill="#F48024"></path></svg></a>
                 </div>
             <nav class="site-footer--nav">
+              <div className="test">
                     <div class="site-footer--col">
                         <h5 class="-title">STACK OVERFLOW</h5>
                         <ul class="-list js-primary-footer-links">
@@ -59,9 +60,8 @@ const Footer = () => {
                         </ul>
                     </div>
                 </div>
-            </nav>
-            <div class="site-footer--copyright fs-fine md:mt24">
-                <ul class="-list -social md:mb8">
+                <div class="site-footer--copyright fs-fine md:mt24">
+                <ul className="test2">
                     <li>Blog</li>
                     <li>Facebook</li>
                     <li>Twitter</li>
@@ -74,10 +74,25 @@ const Footer = () => {
               <span id="svnrev">rev&nbsp;2022.10.25.33519</span>
                 </p>
             </div>
+                </div>
+                </nav>
+            
         </div>
     </footer>
     <style jsx>
       {`
+      .test2 {
+        border: 1px solid blue;
+        display: flex;
+      }
+      .test {
+        border: solid 1px red;
+        display: flex;
+        width: 1264px;
+        margin: 0 auto;
+        padding: 32px 12px 12px 12px;
+        justify-content: space-between;
+      }
       .site-footer--container {
         display: flex;
         align-items: flex-start;
@@ -91,6 +106,7 @@ const Footer = () => {
         list-style: none;
         padding-left: 0;
       }
+      
       li{
         margin-top: 5px;
       }
@@ -105,7 +121,6 @@ const Footer = () => {
         background: rgb(29,29,31);
         color: rgb(172,178,183);
         font-size: 10px;
-        
       }
       h5{
         font-weight: bold;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,24 @@
+import Header from "../components/Header";
+import Header2 from "../components/Header2";
+import Github from "../components/Github";
+import Github2 from "../components/Github2";
+import Google from "../components/Google";
+import Google2 from "../components/Google2";
+import SideBar from "../components/SideBar";
+import Footer from "../components/Footer";
+
+
 export default function Home() {
   return (
     <div>
-      <button type="button">버튼43333333</button>
+      <Header />
+      <Header2 />
+      <Google />
+      <Google2 />
+      <Github />
+      <Github2 />
+      {/* <SideBar /> */}
+      <Footer />
     </div>
   );
 }
