cf8563879fce2db00bc3e74fe642574fec90d2d8
sharon-youn <jos9187@gmail.com>
2022-11-03 10:54:02 +0900

    시작화면 - 변수바뀌는 효과

---
+++ client/pages/hello/cs25.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CS25 from '../../components/CS25'
+
+export default function cs25() {
+  return (
+    <div>
+      <CS25/>
+    </div>
+  )
+}
--- client/pages/hello/start.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-import Image from 'next/image';
-
-
-export default function Hello() {
-  return (
-  <div></div>
-  );
-}
